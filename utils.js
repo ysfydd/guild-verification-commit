ee1f56b7ad9df2867fc2f87c9220cb107e039b37
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:28:00 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:27:56.206Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-12T15:27:59.791Z
+Commit: Fix CSS styling bug on mobile devices
