c86758250ccd544b486edb086038674291e66dfa
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:19:08 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:19:02.616Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-12T15:19:08.308Z
+Commit: Add export functionality for reports
