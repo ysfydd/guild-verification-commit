0503f1d8e6d713ac7acc270b52d75ef818006e4f
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:18:37 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:18:23.465Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T15:18:36.838Z
+Commit: Fix CSS styling bug on mobile devices
