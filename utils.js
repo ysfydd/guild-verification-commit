eee3bbbed95f077fca27cef6f6f039e5b7d6d5d5
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:18:51 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:18:41.765Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-12T15:18:50.844Z
+Commit: Fix broken links in documentation
