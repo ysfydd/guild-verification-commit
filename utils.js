7efc301c0399af1cede73588597cbad6cb22cd2a
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:18:42 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:18:24.956Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T15:18:41.765Z
+Commit: Fix CSS styling bug on mobile devices
