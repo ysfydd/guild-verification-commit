9e9d150922b1dfd8174a5634b5bab28d35ec405b
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:19:22 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:19:19.020Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T15:19:21.521Z
+Commit: Resolve null pointer exception in user service
