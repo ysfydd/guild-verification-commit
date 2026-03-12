ae23f61c020aa1b6b36db437772444ea415a31d6
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:19:17 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:19:08.308Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T15:19:16.771Z
+Commit: Fix database connection timeout
