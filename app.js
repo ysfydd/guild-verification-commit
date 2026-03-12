1f1209d80c74f4d26b5d5686fb70092f6248c6bf
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:27:57 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:27:43.221Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-12T15:27:57.385Z
+Commit: Add multi-language support
