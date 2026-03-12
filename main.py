f1235397864928293a27b99fa1faa0d05bb8fdfa
ysfydd <166503986+ysfydd@users.noreply.github.com>
2026-03-12 18:28:02 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T15:27:47.142Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-12T15:28:02.370Z
+Commit: Fix broken links in documentation
